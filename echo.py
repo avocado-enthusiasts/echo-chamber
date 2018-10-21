05f354d11eef621a601d179c49d437c068af6fa1
Arvinder Bhathal <abhatha2@uwo.ca>
2018-10-20 22:47:55 -0400

    Change user agent platform to Python and print more details on subreddit submissions

--- echo.py
+++ echo.py
@@ -1,7 +1,14 @@
 import praw
 
-reddit = praw.Reddit(user_agent='linux:echo-chamber:v0.0.1')
+reddit = praw.Reddit(user_agent='Python:echo-chamber:v0.0.1')
 
-print(reddit.read_only)
-for submission in reddit.subreddit('learnpython').hot(limit=10):
-  print(submission.title)
+print('Authorized:',str(not reddit.read_only))
+
+subreddit = reddit.subreddit('EnoughTrumpSpam')
+
+for submission in subreddit.hot(limit=10):
+    print(submission.title)  # Output: the submission's title
+    print(submission.score)  # Output: the submission's score
+    print(submission.id)     # Output: the submission's ID
+    print(submission.url)    # Output: the URL the submission points to
+                             # or the submission's URL if it's a self post
