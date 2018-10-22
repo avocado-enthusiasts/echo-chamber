36e9a6c232958822569ae2dc680dc6ee4ae7a5c2
Arvinder Bhathal <abhatha2@uwo.ca>
2018-10-22 12:57:23 -0400

    Implement initial comment/reply gathering

--- src/echo.py
+++ src/echo.py
@@ -1,14 +1,41 @@
 import praw
+from collections import defaultdict
 
-reddit = praw.Reddit(user_agent='Python:echo-chamber:v0.0.1')
+def get_items(comment, item_list):
+  item_list.append(comment)
+  if not hasattr(comment, 'replies'):
+    replies = comment.comments()
+  else:
+    replies = comment.replies
+  for child in replies:
+    get_items(child, item_list)
 
-print('Authorized:',not reddit.read_only)
+def get_ids(r, subreddit_name, count, submissions):
+  subreddit = r.subreddit(subreddit_name)
+  for submission in subreddit.hot(limit=count):
+    submissions.append(submission)
 
-subreddit = reddit.subreddit('EnoughTrumpSpam')
+def process_submissions(item_map, submissions):
+  for submission in submissions:
+    print('Processing submission', submission.id)
+    for comment in submission.comments:
+      get_items(comment, item_map[submission.id])
 
-for submission in subreddit.hot(limit=10):
-    print(submission.title)  # Output: the submission's title
-    print(submission.score)  # Output: the submission's score
-    print(submission.id)     # Output: the submission's ID
-    print(submission.url)    # Output: the URL the submission points to
-                             # or the submission's URL if it's a self post
+def main():
+  r = praw.Reddit(user_agent='Python:echo-chamber:v0.0.1')
+  subreddit_name = 'EnoughTrumpSpam'
+  item_map = defaultdict(list)
+  submissions = []
+  count = 10
+
+  if r.read_only:
+    print('Not authorized!')
+    raise NotImplementedError
+
+  get_ids(r, subreddit_name, count, submissions)
+  process_submissions(item_map, submissions)
+
+  print(item_map)
+
+if __name__ == '__main__':
+  main()
