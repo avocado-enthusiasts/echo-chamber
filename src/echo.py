aeeea7f0ef0a0396dbdfd42d36fe8eb87159a3fc
Arvinder Bhathal <abhatha2@uwo.ca>
2018-11-21 20:30:28 -0500

    Get comment scraping working

--- src/echo.py
+++ src/echo.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-import praw, json, requests
+import praw, json, requests, io, os, pathlib, pprint
 from collections import defaultdict
-from timeit import default_timer as timer
 
 def get_items(comment, item_list):
   item_list.append(comment)
@@ -31,18 +30,46 @@ def main():
   if r.read_only:
     print('Not authorized!')
     return
-
-  req = requests.get('https://api.pushshift.io/reddit/submission/comment_ids/9xwtzs')
-  ids = req.json()
-  start = timer()
   count = 0
-  for cid in ids['data']:
-    body = r.comment(cid).body
-    if body != '[deleted]' and body != '[removed]':
-      count += 1
-      print(body+'\n')
-  end = timer()
-  total = end - start
+  with open('subreddit_list.txt') as subreddit_list:
+    for line in subreddit_list:
+      subreddit_name = line.strip()
+      subreddit = r.subreddit(subreddit_name)
+      home_dir = str(pathlib.Path.home())
+      subreddit_dir = home_dir + '/reddit/' + subreddit_name
+      os.makedirs(subreddit_dir, exist_ok = True)
+      print(subreddit_name)
+      for submission in subreddit.hot(limit=1000):
+        submission_id = submission.id
+        print(submission_id)
+        submission_file = subreddit_dir + '/' + submission_id
+        with open(submission_file, 'w') as sub_file:
+          req_path = 'https://api.pushshift.io/reddit/submission/comment_ids/' + submission_id
+          req = requests.get(req_path)
+          ids = req.json()
+          for cid in ids['data']:
+            body = r.comment(cid).body
+            if body and body != '[deleted]' and body != '[removed]':
+              sub_file.write(body + '/n')
+        
+
+      # print(subreddit.display_name)
+      # pprint.pprint(vars(subreddit))
+
+      # print(subreddit.display_name)
+      # print(subreddit.subscribers)
+
+
+  # req = requests.get('https://api.pushshift.io/reddit/submission/comment_ids/9xwtzs')
+  # ids = req.json()
+  # start = timer()
+  # count = 0
+  # for cid in ids['data']:
+  #   body = r.comment(cid).body
+  #   if body and body != '[deleted]' and body != '[removed]':
+  #     print(body+'\n')
+  # end = timer()
+  # total = end - start
   # subreddit_name = 'EnoughTrumpSpam'
   # item_map = defaultdict(list)
   # submissions = []
