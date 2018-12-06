0aa5d37d90331eea6910d0ca37302fb311a51c5f
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-06 17:22:02 -0500

    update init script

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,5 +1,26 @@
+from trainer.preprocessor import Strip
 from trainer.splitter import Splitter
+import json
 
 if __name__ == '__main__':
-    splitter = Splitter(0.8, 0.05, 0.15, "data/anarchism")
+    corpora = "data/corpora"
+    normalized = "data/normalized"
+    tease = Strip()
+    print("Starting...")
+    output = open(normalized, "w+")
+    with open(corpora) as textSet:
+        i = 0
+        for line in textSet:
+            clean_comment = tease.removeAllJSON(json.loads(line), 'body')
+            json.dump(clean_comment, output)
+            output.write('\n')
+            print("Cleaned comment #" + str(i))
+            i += 1
+    textSet.close()
+    output.close()
+    print("Cleaning finished!")
+    splitter = Splitter(0.8, 0, 0.2, normalized)
     splitter.doSplit()
+    print("Splitting finished!")
+    splitter.saveSets()
+    print("Sets saved!")
