8e8c875f1399ab1d2f15b47490ac72b916e03291
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-07 12:56:33 -0500

    add peak.py

---
+++ trainer/peak.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from collections import defaultdict
+
+
+cos_list = os.listdir(sys.argv[1])
+cos = []
+dic = defaultdict(int)
+for file in cos_list:
+  file = open(sys.argv[1] + file)
+  for line in file:
+    if line:
+      line = line.strip()
+      dic[line] += 1
+  sorted_dic = sorted(dic.items(), key=lambda x: x[1])
+  print(sorted_dic)
