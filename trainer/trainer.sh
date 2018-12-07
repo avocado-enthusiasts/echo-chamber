c7106652740e0499fcb8ed5c7a79eb42006e8685
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-07 10:13:43 -0500

    update trainer.sh

--- trainer/trainer.sh
+++ trainer/trainer.sh
@@ -2,6 +2,7 @@
 # $1 is the address of fasttext repository.
 # $2 is the address of train set
 # $3 is the address of test set
+# $4 is the name of the category
 train=$2
 testSet=$3
 for j in `seq 1 5`;
@@ -9,7 +10,7 @@ do
         for k in 1 2 3;
         do
                 let "DIM = $j * 50"
-                name="model_dim$DIM-ngram$k"
+                name="$4_model_dim$DIM-ngram$k"
                 echo "$name..."
                 echo "Model $name.bin - DIM: $DIM: - Ngrams: $k" &>> ../output/results/report.txt
                 $1 sent2vec -input "$train" -output "../output/models/$name" -dim $DIM -wordNgrams $k &>> ../output/results/report.txt
