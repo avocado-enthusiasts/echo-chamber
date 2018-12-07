c485560c496162fffe211c404d2ef23d02c46490
Amir Haghighati <haghighati.amir@gmail.com>
2018-12-07 09:49:38 -0500

    update trainer script

--- trainer/trainer.sh
+++ trainer/trainer.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # $1 is the address of fasttext repository.
-for i in 1 5 9;
+# $2 is the address of train set
+# $3 is the address of test set
+train=$2
+testSet=$3
+for j in `seq 1 5`;
 do
-        for j in `seq 1 5`;
+        for k in 1 2 3;
         do
-                for k in 1 2 3;
-                do
-                        LR="0.$i"
-                        let "DIM = $j * 50"
-                        name="model_lr$LR-dim$DIM-ngram$k"
-                        echo "$name..."
-                        echo "Model $name.bin - LR: $LR - DIM: $DIM: - Ngrams: $k" &>> ../data/output/report.txt
-                        $1 sent2vec -input ../data/train.txt -output "../data/models/$name" -lr $LR -dim $DIM -wordNgrams $k &>> ../data/output/report.txt
-                        echo "Learned..."
-                        cat "../data/test.txt" | $1 print-sentence-vectors "../data/models/$name.bin" &>> "./output/$name.txt"
-                        echo "=====================================================================================================================\n"&>> ./output/report.txt
-                        echo "And Tested..."
-                        rm "./models/$name.bin"
-                        echo "And also removed!"
-                done
+                let "DIM = $j * 50"
+                name="model_dim$DIM-ngram$k"
+                echo "$name..."
+                echo "Model $name.bin - DIM: $DIM: - Ngrams: $k" &>> ../data/output/report.txt
+                $1 sent2vec -input "$train" -output "../data/models/$name" -lr $LR -dim $DIM -wordNgrams $k &>> ../data/output/report.txt
+                echo "Learned..."
+                cat "$testSet" | $1 print-sentence-vectors "../data/models/$name.bin" &>> "./output/$name.txt"
+                echo "=====================================================================================================================\n"&>> ./output/report.txt
+                echo "And Tested..."
+                #rm "./models/$name.bin"
+                #echo "And also removed!"
         done
 done
